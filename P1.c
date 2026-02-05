dfbd40449c20a961072f288ebfb73792462aa6e6
Prapti711 <patprapti.11@gmail.com>
2026-02-05 20:36:05 +0530

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    printf("\n Arithmatic Operation:");
+
+    int a;
+    int b;
+
+    printf("\n enter value of a:");
+    scanf("%d",& a);
+    printf("\n enter value of b:");
+    scanf("%d",& b);
+    printf("\n sum is:%d",a+b);
+    printf("\n sub is:%d",a-b);
+    printf("\n multi is:%d",a*b);
+    printf("\n div is:%d",a/b);
+    printf("\n mod is:%d",a%b);
+
+    return 0;
+}
+
+
